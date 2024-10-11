3cc7907d4543dda79134b6faeca02c22bc160130
eveki <eveki@10.220.35.53>
2024-10-10 20:04:45 -0400

    pa02

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,62 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
 public class PA2a {
 
-	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		int positiveSum=0;
+		int negativeSum=0;
+		int totalSum=0;
+		int positiveCount=0;
+		int negativeCount=0;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter five whole numbers: ");
+		for(int i=0;i<5;i++) {
+			int num=scanner.nextInt();
+			totalSum+=num;
+			
+			if (num>0) {
+				positiveSum+=1;
+				positiveCount++;
+			} else {
+				negativeSum+=1;
+				negativeCount++;
+			}
+		}
+		double positiveAverage=positiveSum/positiveCount;
+		double negativeAverage=negativeSum/negativeCount;
+		double totalAverage=totalSum/5;
+		
+	if (positiveCount != 1) {
+            System.out.printf("The sum of the %d positive numbers: %d%n" ,positiveCount, positiveSum);
+     }
+		else {
+            System.out.printf("The sum of the %d positive number: %d%n" ,positiveCount, positiveSum);
+     }
+	if (negativeCount != 1) {
+            System.out.printf("The sum of the %d non-positive numbers: %d%n" ,negativeCount, negativeSum);
+     }
+     else {
+            System.out.printf("The sum of the %d non-positive number: %d%n" ,negativeCount, negativeSum);
+     }
+     System.out.printf("The sum of the 5 numbers: %d%n" , totalSum);
+    if (positiveCount != 1) {
+            System.out.printf("The average of the %d positive numbers: %.2f%n",positiveCount, positiveAverage);
+     }
+     else {
+            System.out.printf("The average of the %d positive number: %.2f%n",positiveCount, positiveAverage);
+     }
+    if (negativeCount != 1) {
+            System.out.printf("The average of the %d non-positive numbers: %.2f%n",negativeCount, negativeAverage);
+     }
+     else {
+            System.out.printf("The average of the %d non-positive number: %.2f%n",negativeCount, negativeAverage);
+     }
+     System.out.printf("The average of the 5 numbers: %.2f%n", totalAverage);
 	}
-
 }
+
+
+
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,36 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
 public class PA2b {
 
-	// TODO: document this function
+
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a b and c: ");
+		double a=scanner.nextDouble();
+		double b=scanner.nextDouble();
+		double c=scanner.nextDouble();
+		
+		double discriminant=b*b-4*a*c;
+		if (discriminant<0) {
+			System.out.printf("Roots : Imaginary");
+		}
+		else if(discriminant==0) {
+			double root=-b/2*a;
+			System.out.print("Root: %.2f%n"+root);
+		}
+		else {
+			 double root1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+	         double root2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+	         if(root1<root2) {
+	        	 System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+	         }
+	         else {
+	        	 System.out.printf("Roots: %.2f, %.2f%n", root2, root1);
+	         }
+	         }
+		}
 	}
 
-}
+
+
